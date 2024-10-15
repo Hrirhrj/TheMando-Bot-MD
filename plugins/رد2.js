5692f8907125db2f29f15c6d6455814037b59c24
Hrirhrj <bwt464451@gmail.com>
2024-10-15 10:12:42 +0300

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*مــنــور يـا قــلــب مــانــدو لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
+    let message = '* شبيڪ لؤبيڪ مآندؤ ؤسؤنيڪ بين آيديڪ⚡️* ; 
 await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
     conn.sendFile(m.chat, 'https://envs.sh/Bre.jpg', 'image.jpg', message, m);
 };
